00dec1517b923b6cff53afc4e72777518840f66d
noble <noblewill2012@icloud.com>
2020-12-13 16:06:25 +0100

    unsplash api call

---
+++ src/App.vue
@@ -0,0 +1,84 @@
+<template>
+  <div id="app">
+    <div class="header"></div>
+    <div class="container">
+      <div v-if="errored">Sorry, something went wrong, try again.</div>
+
+      <div v-else>
+        <div>
+          <input type="text" placeholder="Search for a Photo" />
+        </div>
+        <div v-if="loading">loading...</div>
+
+        <div v-else>
+          <div class="photos">
+            <div v-for="(photo, idx) in info" :key="idx" class="photo">
+              <img :src="photo.urls.small" alt="" />
+              <div class="photo-data">
+                <div class="photo-name"></div>
+                <div class="photo-author">{{ photo.width }}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      info: null,
+      loading: false,
+      errored: false,
+    };
+  },
+
+  mounted() {
+    axios
+      .get(
+        "https://api.unsplash.com/photos/?client_id=Tbt93pi1s83atkRNJPIkLrIKkjCksONI8QYzGnW1JE0"
+      )
+      .then((response) => {
+        this.info = response.data;
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+        this.errored = true;
+      })
+      .finally(() => (this.loading = false));
+  },
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+
+.container {
+  padding: 5rem 5rem;
+}
+
+.header {
+  background: #dce2e9;
+  height: 35%;
+  width: 100vw;
+  position: absolute;
+  z-index: -1;
+}
+
+.photos {
+  width: 100vw;
+  display: grid;
+  padding: 0 5rem;
+  justify-content: center;
+  grid-template-columns: 1fr 1fr 1fr;
+}
+</style>
